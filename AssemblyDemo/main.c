cbbf36531e19bcfbc54efb0a16da1974941c883c
akhzar nazir <akhzarn@yahoo.com>
2018-01-26 14:26:59 +0500

    Initial Commit

---
+++ AssemblyDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AssemblyDemo
+//
+//  Created by Akhzar Nazir on 26/01/2018.
+//  Copyright © 2018 Akhzar Nazir. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
